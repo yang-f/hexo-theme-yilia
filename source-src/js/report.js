3a2fa291fb88eaee952a6d5d2aea7a1232aebd8a
yangfei <yfei89@126.com>
2017-07-05 16:22:33 +0800

    comments style

--- source-src/js/report.js
+++ source-src/js/report.js
@@ -1,12 +1,12 @@
-var jsCookie = require('js-cookie')
+// var jsCookie = require('js-cookie')
 
 // require('badjs-report')
 
-function getQueryString(name) { 
-	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
-	var r = window.location.search.substr(1).match(reg); 
-	if (r != null) return unescape(r[2]); return null; 
-} 
+// function getQueryString(name) { 
+// 	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+// 	var r = window.location.search.substr(1).match(reg); 
+// 	if (r != null) return unescape(r[2]); return null; 
+// } 
 // 统计用，开发者不需要理会
 // if (window.BJ_REPORT) {
 // 	BJ_REPORT.init({
